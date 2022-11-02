229f953122fdcb518ab3ddc34fb6c57a91ed6189
fel-w <wangottafelix@gmail.com>
2022-11-02 08:12:11 +0300

    Initialize middleware

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
 
 // get body json
 app.use(bodyParser.json());
 
 // initialize routes
 app.use('/api/ninjas', require('./routes/router'));
+
+// error handling middle ware
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(422).send({'error':err._message});
+})
 
 // listen for requests
 app.listen(process.env.port || 4000, () => {
